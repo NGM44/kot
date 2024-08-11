8da19c72b619d4bf5dda5abcb9baf85c2be0871f
Sahil Menda <sahilymenda@gmail.com>
2024-08-12 00:55:20 +0530

    ip

--- src/pages/dashboard/RabbitMQConsumer.tsx
+++ src/pages/dashboard/RabbitMQConsumer.tsx
@@ -6,7 +6,7 @@ const RabbitMQConsumer = () => {
   const [messages, setMessages] = useState<IWeatherData>();
 
   useEffect(() => {
-    const ws = new WebSocket(`ws://api.sensormagics.com:8080`);
+    const ws = new WebSocket(`ws://128.199.21.134:8080`);
 
     ws.onopen = () => {
       console.log('Connected to WebSocket server');
